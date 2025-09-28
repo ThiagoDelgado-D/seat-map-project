7f64f876bfbb4b197a95b9b80777800f6165e5ce
Thiago <thiagodelgado.dev@gmail.com>
2025-09-28 20:09:14 -0300

    refactor: streamline Home component structure by removing unnecessary div wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,44 +3,45 @@ import { features_section, renderer_features } from "@/constants/features";
 import { TRUSTED_COMPANIES } from "@/constants/logos";
 import { SEAT_MAP_STATS } from "@/constants/stats";
 
-
 export default function Home() {
   return (
     <>
-      <div>
-        <HeroSection />
-        <StatsSection
-          stats={SEAT_MAP_STATS}
-          title="Seats.io in numbers"
-        />
-        <LogosSection
-          logos={TRUSTED_COMPANIES}
-          title="They trust us."
-          animationSpeed="normal"
-        />
-        <FeatureSection
-          title="Floor plan designer"
-          subtitle="Design beautiful charts."
-          imageUrl="/sit-manager.png"
-          features={features_section}
-        />
-        <FeatureSection
-          title="Floor plan renderer"
-          subtitle="Outstanding ticket buying experience."
-          imageUrl="/renderer@2x.png"
-          features={renderer_features}
-          variant="default"
-          imagePosition="center"
-          ctaText="See documentation"
-        />
-        <CTASection
-          title="Start integrating today."
-          subtitle="Free testing account with technical support for integration."
-          planName="Pricing plans"
-          ctaText="Sign up"
-          variant="default"
-        />
-      </div>
+      <HeroSection />
+      <StatsSection
+        stats={SEAT_MAP_STATS}
+        title="Seats.io in numbers"
+      />
+      <LogosSection
+        logos={TRUSTED_COMPANIES}
+        title="They trust us."
+        animationSpeed="normal"
+      />
+      <FeatureSection
+        id="features"
+        title="Floor plan designer"
+        subtitle="Design beautiful charts."
+        imageUrl="/sit-manager.png"
+        features={features_section}
+        ctaText="Try the Editor"
+      />
+      <FeatureSection
+        id="renderer"
+        title="Floor plan renderer"
+        subtitle="Outstanding ticket buying experience."
+        imageUrl="/renderer@2x.png"
+        features={renderer_features}
+        variant="default"
+        imagePosition="center"
+        ctaText="See Live Demo"
+      />
+      <CTASection
+        id="pricing"
+        title="Start integrating today."
+        subtitle="Free testing account with technical support for integration."
+        planName="Pricing plans"
+        ctaText="Sign up"
+        variant="default"
+      />
     </>
   );
 }
